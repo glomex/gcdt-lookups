e65f9d5690b77d5da5b77b1374c37e82c4098454
Jenkins Continuous Integration Server <sre-chapter@de.glomex.com>
2017-08-11 04:37:07 +0000

    Bump version: 0.0.18 → 0.0.19

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' no
 
 setup(
     name='gcdt-lookups',
-    version='0.0.18',
+    version='0.0.19',
     description='Plugin (gcdt-lookups) for gcdt',
     long_description=long_description,
     license='MIT',
